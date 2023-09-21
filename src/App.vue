802eb820b8127e8c1819d58a1e456428a6518c17
wanglei <wanglei_yf@piesat.cn>
2023-09-21 09:33:39 +0800

    chore: init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <ConfigProvider :locale="zhCN">
+    <router-view />
+  </ConfigProvider>
+</template>
+
+<script setup lang="ts">
+import { ConfigProvider } from 'ant-design-vue';
+import zhCN from 'ant-design-vue/es/locale/zh_CN';
+</script>
+
+<style lang="scss" scpoed>
+#app {
+  font-family: Avenir, Helvetica, 'PingFang SC', 'Microsoft YaHei', 'WenQuanYi Micro Hei',
+    sans-serif;
+  font-size: 0.16rem;
+  color: #2c3e50;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
