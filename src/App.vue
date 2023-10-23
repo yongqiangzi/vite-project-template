5f55a0ba14222e3ca7297b49b1ae7af4735c83f4
wanglei <wanglei_yf@piesat.cn>
2023-10-23 14:18:39 +0800

    fix: 修改系统默认字体

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: wanglei
+ * @Date: 2023-09-20 17:39:31
+ * @LastEditors: wanglei
+ * @LastEditTime: 2023-10-23 14:18:01
+ * @FilePath: /src/App.vue
+ * @Descripttion:
+-->
 <template>
   <ConfigProvider :locale="zhCN">
     <router-view />
@@ -11,8 +19,9 @@ import zhCN from 'ant-design-vue/es/locale/zh_CN';
 
 <style lang="scss" scpoed>
 #app {
-  font-family: Avenir, Helvetica, 'PingFang SC', 'Microsoft YaHei', 'WenQuanYi Micro Hei',
-    sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial,
+    'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol',
+    'Noto Color Emoji';
   font-size: 0.16rem;
   color: #2c3e50;
   -webkit-font-smoothing: antialiased;
